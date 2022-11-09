beb81ed9b5903bf5bf2a40967dedf290e1f467a5
harisanamotivitylabs <107044790+harisanamotivitylabs@users.noreply.github.com>
2022-11-09 11:36:23 +0530

    Icons addded

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => (
         <Route path='/' element={<DefaultLayout/>}>
           <Route index element={<HomePage />} />
           <Route path='doctor' element={<Doctor />} >
+            
           </Route>
           <Route path = 'patient' element={<Patient/>}>
           </Route>
--- src/layouts/default_layout/components/footer/index.js
+++ src/layouts/default_layout/components/footer/index.js
@@ -6,30 +6,14 @@ import "./footer.css"
 const Footer = () => {
   return (
     <>
-      <footer className='footer-bs'>
-        <div className="container min-vw-100 bg-light ">
-          <div className="row">
-            <div className="col-xl-3 col-lg-4 col-md-6">
-              <div>
-                <h3> <Link to="home" className="text-decoration-none"><img className='img w-75 p-3' src='https://eyecarehelpline.com/hospdt/032019/12/186/max.png' alt='logo' /></Link></h3>
-                <p className="mb-30 ">
+      <footer className='container  '>
 
-                  <h5>
-                    Social Media Networks
-                  </h5>
-
-                  <div>
-                    <span class="link"> <i class="bi bi-facebook " style={{color:'blue'}}></i></span>
-                    <span class="link"> <i class="bi bi-google " style={{color:'red'}}  ></i></span>
-                    <span class="link"> <i class="bi bi-twitter " style={{color:'blue'}} ></i></span>
-                    
-                    <span class="link">  <i class="bi bi-instagram " style={{color:'#c40898'}}  ></i></span>
-                  </div>
-                </p>
-              </div>
-            </div>
-            <div className="col-xl-2 offset-xl-1 col-lg-2 col-md-6">
-
+        <div className='row   py-5'>
+          <div className='col-md-3 col-sm-6 col-12'>
+            <Link to="home" className="text-decoration-none"><img className='img w-75 p-3' src='https://eyecarehelpline.com/hospdt/032019/12/186/max.png' alt='logo' /></Link>
+          </div>
+          <div className='col-md-3 col-sm-6 col-12 '>
+            
               <h4>Quick Links</h4>
               <ul className="list-unstyled">
                 <li>
@@ -39,63 +23,48 @@ const Footer = () => {
                   <Link to="aboutus" className="text-decoration-none">About Us</Link>
                 </li>
                 <li>
-                  <Link to="service" className="text-decoration-none">Service</Link>
+                  <Link to="feedback" className="text-decoration-none">Feedback</Link>
                 </li>
                 <li>
                   <Link to="contact" className="text-decoration-none">Contact</Link>
                 </li>
               </ul>
+           
+          </div>
+          <div className='col-md-3 col-sm-6 col-12'>
+            <h4>Address</h4>
 
-            </div>
-            <div className="col-xl-3 col-lg-3 col-md-6">
-              <div>
-                <h4>Service</h4>
-                <ul className="list-unstyled">
-                  <li>
-                    <Link to="marketing" className="text-decoration-none">Medical</Link>
-                  </li>
-                  <li>
-                    <Link to="brandin" className="text-decoration-none">International specialists</Link>
-                  </li>
-                  <li>
-                    <Link to="webdesign" className="text-decoration-none">Latest Technology Equipment </Link>
-                  </li>
-                  <li>
-                    <Link to="graphics" className="text-decoration-none">Therapy Services</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-xl-3 col-lg-3 col-md-6">
-                <h4>Address</h4>
-                <ul className="list-unstyled">
-                  <p>Dallas Center, 6th & 7th Floor, 83/1, Plot No A1, Knowledge City Rd, Rai Durg, Hyderabad, Telangana 500032.</p>
-                  <li>
-                    <p>+91 888-555-4444 / 888-555-6666</p>
-                   
-                  </li>
-                  <li>
-                    <p><Link to="mail">maxhospital@gmail.com</Link>
-                    </p> 
-                  </li>
-                  <li>
-                    <p>
-                      <ul>
-
-                      </ul>
-                    </p>
-                  </li>
-                </ul>
-              
+            <address>
+              Dallas Center, 6th & 7th Floor:<br></br>
+              Knowledge City Rd :<br></br>
+              Rai Durg, Hyderabad:<br></br>
+              Telangana 500032.<br></br>
+              India
+            </address>
+          </div>
+          <div className='col-md-3 col-sm-6 col-12'>
+            <div>
+              <h4>Follow Us</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <p>+91 888-555-4444 / 888-555-6666</p>
+                </li>
+                <li>
+                  <Link to="mail">maxhospital@gmail.com</Link>
+                </li>
+              </ul>
+              <p>
+                Social Media Networks
+              </p>
+              <span class="link"> <i class="bi bi-facebook " style={{ color: 'blue' }}></i></span>
+              <span class="link"> <i class="bi bi-google " style={{ color: 'red' }}  ></i></span>
+              <span class="link"> <i class="bi bi-twitter " style={{ color: 'blue' }} ></i></span>
+              <span class="link">  <i class="bi bi-instagram " style={{ color: '#c40898' }}  ></i></span>
             </div>
           </div>
-          <div className="d-flex justify-content-center border-top">
-
-            <div className="copyright  ">
-              <p>Developed and maintained by <Link to="/home" target="_blank">company</Link></p>
-
-              <p>Copyright Company Name © 2022. All rights reserved.</p>
-            </div>
+          <div className=' d-flex justify-content-between py-4 my-4 border-top'>
+            <p>Developed and maintained by <Link to="/home" target="_blank">company</Link></p>
+            <p className='copyright d-flex'>Copyright Company Name © 2022. All rights reserved.</p>
           </div>
         </div>
       </footer>

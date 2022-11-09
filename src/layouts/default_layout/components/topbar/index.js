3d99677a317d9792e16434657c0dd37bc83a4a68
sandeepkumarreddymallidi <106801934+sandeepkumarreddymallidi@users.noreply.github.com>
2022-11-09 17:55:13 +0530

    in-navbar-home-removed

--- src/layouts/default_layout/components/topbar/index.js
+++ src/layouts/default_layout/components/topbar/index.js
@@ -8,15 +8,15 @@ const TopBar = () => {
 <header>
 <nav className="navbar navbar-expand-md bg-light">
   <div className="container">
-    <Link className="navbar-brand" href="#">Navbar</Link>
+    <Link className="navbar-brand" to='/'>Navbar</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
       <ul className="navbar-nav">
-        <li className="nav-item mx-2">
+        {/* <li className="nav-item mx-2">
           <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
-        </li>
+        </li> */}
          <li className="nav-item mx-4">
           <Link className="nav-link active" aria-current="page" to='/login'>Login</Link>
         </li>
